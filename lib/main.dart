8c933309412ee62300df15ff0f4fc25b569d788a
Wagner.Araujo <araujowp@gmail>
2021-07-27 13:30:54 -0300

    criando state para envolver a aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:shop/views/products_details_screen.dart';
 import './views/products_overview_screen.dart';
 import './utils/app_routes.dart';
+import './providers/counter_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange, 
-        fontFamily: 'Lato',
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Minha Loja',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange, 
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailsScreen()
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailsScreen()
-      },
     );
   }
 }
--- lib/views/products_details_screen.dart
+++ lib/views/products_details_screen.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
+import '../providers/counter_provider.dart';
 
-class ProductDetailsScreen extends StatelessWidget {
+class ProductDetailsScreen extends StatefulWidget {
 
+  @override
+  _ProductDetailsScreenState createState() => _ProductDetailsScreenState();
+}
+
+class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     final Product product = ModalRoute.of(context).settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
+      ),
+      body: Column(
+        children: [
+          Text(CounterProvider.of(context).state.value.toString()),
+          ElevatedButton(
+            child: Text('+'),
+            onPressed: (){
+              setState(() {
+                CounterProvider.of(context).state.inc();
+              });
+              print(CounterProvider.of(context).state.value);
+            }
+            ), 
+        ],
       ),
     );
   }
