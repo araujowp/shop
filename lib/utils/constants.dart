b49f08adb9e5f122fcc33d0438ae413964519294
Wagner.Araujo <araujowp@gmail>
2021-10-13 17:46:23 -0300

    add FutureBuilder tela de lista de pedidos

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   static const PRODUCT_BASE_URL =
       'https://shop-coder-ce65e-default-rtdb.firebaseio.com/products';
+  static const ORDER_BASE_URL =
+      'https://shop-coder-ce65e-default-rtdb.firebaseio.com/orders';
 }
